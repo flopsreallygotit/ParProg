7b96bca3cbf45792f82f0f59b657bb3e0efea6a8
flops <flopsreallygotit@gmail.com>
2025-04-22 15:06:36 +0300

    Simple thread communication

--- Lab2/Task/common_benchmark.hpp
+++ Lab2/Task/common_benchmark.hpp
@@ -1,32 +1,29 @@
 #pragma once
 
 #include <chrono>
+#include <utility>
 
-template <void bench_func()> class CommonBenchmark {
+template <auto Func, typename... Args> class CommonBenchmark final {
   public:
     CommonBenchmark(unsigned trials_count = 100)
         : m_trials_count(trials_count) {}
 
-    long get_avg_time() {
-        long sum_time = 0;
+    long get_avg_time(Args &...args) {
+        using clock_t = std::chrono::high_resolution_clock;
+        using time_t = std::chrono::nanoseconds;
 
-        for (unsigned trial = 0; trial < m_trials_count; ++trial)
-            sum_time += measure_time();
+        long sum_time = 0;
+        for (unsigned trial = 0; trial < m_trials_count; ++trial) {
+            clock_t::time_point start = clock_t::now();
+            Func(std::forward<Args>(args)...);
+            clock_t::time_point end = clock_t::now();
+
+            sum_time += std::chrono::duration_cast<time_t>(end - start).count();
+        }
 
         return (sum_time / m_trials_count);
     }
 
   private:
-    long measure_time() {
-        using clock_t = std::chrono::high_resolution_clock;
-        using time_t = std::chrono::nanoseconds;
-
-        clock_t::time_point start = clock_t::now();
-        bench_func();
-        clock_t::time_point end = clock_t::now();
-
-        return std::chrono::duration_cast<time_t>(end - start).count();
-    }
-
     unsigned m_trials_count = 0;
 };
--- Lab2/Task/thread_communication.cpp
+++ Lab2/Task/thread_communication.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
+#include <thread>
 
 #include "common_benchmark.hpp"
 
-static void thread_communicator() {}
+struct StringBuffer {
+    bool m_modified = false;
+    std::string m_content;
+
+    StringBuffer(size_t length) : m_content(length, 0) {}
+
+    void write(const std::string &message) {
+        m_modified = true;
+        m_content = message;
+    }
+
+    std::string read() { return m_content; }
+};
+
+static void speak(StringBuffer *buffer) {
+    const std::string message = "MSG";
+    buffer->write(message);
+}
+
+static void listen(StringBuffer *buffer) {
+    while (!buffer->m_modified) {
+    }
+}
+
+void thread_communicator(StringBuffer *common_buffer) {
+    std::thread speaker = std::thread{speak, common_buffer};
+    std::thread listener = std::thread{listen, common_buffer};
+
+    speaker.join();
+    listener.join();
+}
 
 int main() {
-    CommonBenchmark<thread_communicator> benchmark{};
+    StringBuffer *common_buffer = new StringBuffer{256};
+    CommonBenchmark<thread_communicator, StringBuffer *> benchmark{};
 
-    long avg_time = benchmark.get_avg_time();
+    long avg_time = benchmark.get_avg_time(common_buffer);
 
     std::cout << "Average thread communication time is `" << avg_time
               << "` ns;\n";
+
+    delete common_buffer;
 
     return 0;
 }
